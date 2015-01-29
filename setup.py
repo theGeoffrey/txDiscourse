d7866c3a2fbd4c445fec2fd93ebe0567243ba8ac
anoukruhaak <anoukruhaak@gmail.com>
2015-01-29 13:56:58 +0000

    new url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='txDiscourse',
-    version='0.1.1',
+    version='0.2',
     description='Discourse API wrapper for Twisted',
     classifiers=[
         'Programming Language :: Python :: 2.7',
@@ -10,14 +10,14 @@ setup(
     ],
     maintainer='Anouk Ruhaak',
     license='APL2',
-    url='https://github.com/theGeoffrey/tx_discourse_api',
+    url='https://github.com/theGeoffrey/txDiscourse',
     long_description=open('README.md').read(),
     packages=['txDiscourse'],
     install_requires=[
         'Twisted >= 14.0.0',
         'treq==0.2.1'
     ],
-
+    email='anoukruhaak@gmail.com',
     tests_require=[
         'nosetests == 1.3.4'
     ]
